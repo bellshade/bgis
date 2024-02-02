57741cc32439a7fc4e703fd6f240931faf8b3e66
slowy07 <slowy.arfy@gmail.com>
2024-02-02 18:40:46 +0700

    chore: merubah struktur folder
    menambahkan beberapa contoh serta
    menambahkan fungsi untuk memantau pengguna pada peta
    
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ bgis_example/interaktif_user.py
@@ -0,0 +1,20 @@
+import bgis
+from ipywidgets import Label
+from ipyleaflet import Marker
+
+Map = bgis.mapping(center=(40, -100), zoom=4)
+label = Label()
+coordinates = []
+
+
+def handle_interaction(**kwargs):
+    latlon = kwargs.get("coordinates")
+    if kwargs.get("type") == "mousemove":
+        label.value = str(latlon)
+    elif kwargs.get("type") == "click":
+        coordinates.append(latlon)
+        Map.add_layer(Marker(location=latlon))
+
+
+Map.on_interaction(handle_interaction)
+print(Map)
